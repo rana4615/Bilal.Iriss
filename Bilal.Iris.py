6a3870b02119fe5788ddd4c4ee41aaa62f8b2b53
rana4615 <132162476+rana4615@users.noreply.github.com>
2023-05-01 00:01:11 +0500

    Add files via upload

---
+++ Bilal.Iris.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[4]:
+
+
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+
+
+# In[5]:
+
+
+iris=pd.read_csv("C:\\Users\\My Pc\\Downloads\\iris.csv")
+
+
+# In[6]:
+
+
+iris
+
+
+# In[9]:
+
+
+iris.plot()
+plt.grid()
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
